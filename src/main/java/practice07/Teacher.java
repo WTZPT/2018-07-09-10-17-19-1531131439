ad492dab5bfd7a6f52c017e945e7a1ff614cf4d3
hardy <hard.t.z.wei@oocl.com>
2020-07-22 16:17:47 +0800

    [practice7][Hardy] feature: 1.Add name and age to Person 2.Add introduce method to Person 3.Add number to Klass 4.Add getDisplayName method to Klass 5.Update Student extends Person 6.Add Klass to Student 7.Overwrite introduce method in Student 8.Update Teacher extends Person 9.Add klass to Teacher 10.Override introduce method in Teacher 11.Add introduceWith method to Teacher

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,35 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name,age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Teacher. " + (klass != null ? "I teach Class "+this.klass.getNumber()+"." : "I teach No Class.");
+    }
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber() == this.klass.getNumber()) {
+            return super.introduce()+" I am a Teacher. "+"I teach "+ student.getName()+".";
+        }
+        return super.introduce()+" I am a Teacher. "+"I don't teach "+ student.getName()+".";
+    }
+
 }
