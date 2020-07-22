217db89e29114f85882ed292d20ff31e0f43bd60
hardy <hard.t.z.wei@oocl.com>
2020-07-22 18:08:06 +0800

    [practice9][Hardy] feature: 1.Add name and age to Person 2.Add introduce method to Person 3.Override equal method in Person 4.Add number and leader to Klass 5.Add getDisplayName method to Klass 6.Add assignLeader and appendMember method to klass 7.Update Student extends Person 8.Add Klass to Student 9.Overwrite introduce method in Student 10.Update Teacher extends Person 11.Add klass to Teacher 12.Override introduce method in Teacher 13.Add introduceWith method to Teacher

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,37 @@
 package practice09;
 
-public class Teacher {
+
+public class Teacher extends Person{
+    private  Klass klass;
+
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public  Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce()+" I am a Teacher. " + (klass != null ? "I teach Class "+this.klass.getNumber()+"." : "I teach No Class.");
+    }
+
+    public String introduceWith(Student student) {
+        if(student.getKlass().getNumber() == this.klass.getNumber()) {
+            return super.introduce()+" I am a Teacher. "+"I teach "+ student.getName()+".";
+        }
+        return super.introduce()+" I am a Teacher. "+"I don't teach "+ student.getName()+".";
+    }
+
 }
