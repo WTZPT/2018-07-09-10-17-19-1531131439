c8cfb2f66983cd5d25579bf533771af8272291ec
hardy <hard.t.z.wei@oocl.com>
2020-07-22 17:36:19 +0800

    [practice8][Hardy] feature: 1.Add name and age to Person 2.Add introduce method to Person 3.Override equal method in Person 4.Add number and leader to Klass 5.Add getDisplayName method to Klass 6.Add assignLeader method to klass 7.Update Student extends Person 8.Add Klass to Student 9.Overwrite introduce method in Student 10.Update Teacher extends Person 11.Add klass to Teacher 12.Override introduce method in Teacher 13.Add introduceWith method to Teacher

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,52 @@
 package practice08;
 
-public class Person {
+import java.util.Objects;
+
+public class Person  {
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is "+this.name+". I am "+this.age+" years old.";
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        Person person = (Person) o;
+        return Objects.equals(id, person.id);
+    }
+
 }
